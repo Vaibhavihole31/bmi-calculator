c6807038a1bdd498cbb580e4fb5184bf4930a97a
Vaibhavihole31 <itspinki05@gmail.com>
2022-02-03 18:16:07 +0530

    Updated files

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function calculateBMI() {
+    var w= parseFloat(document.getElementById("txt-weight").value);
+    var h= parseFloat(document.getElementById("txt-height").value);
+    var hm=h/100;
+    var bmi=(w/(hm * hm)).toFixed(2);
+      
+    document.getElementById("txt-bmi").innerHTML="BMI: "+ bmi;
+}
